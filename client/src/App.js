fdab32edd61818a9443e16bdff8743edf502a61a
Valerie Vazquez <vnvazquez68@gmail.com>
2020-05-07 13:20:18 -0600

    movies show app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { BrowserRouter as Route } from "react-router-dom";
+import MovieList from '../src/Movies/MovieList';
+import Movie from '../src/Movies/Movie';
 import SavedList from './Movies/SavedList';
-
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
-
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -20,17 +20,17 @@ const App = () => {
     }
     getMovies();
   }, []);
-
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
-
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/">
+        <MovieList movies={movieList} />
+      </Route>          
+      <Route path="/movies/:id" component={Movie} />
     </div>
   );
 };
-
 export default App;
